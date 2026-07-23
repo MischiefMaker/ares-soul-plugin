0ef9906f7024d9eba7963787825d76066024e18b
MischiefMaker <59433210+MischiefMaker@users.noreply.github.com>
2026-07-23 17:19:22 -0400

    Complete command parity audit fixes

--- web-portal/app/components/soul/scene-tools.js
+++ web-portal/app/components/soul/scene-tools.js
@@ -4,6 +4,22 @@ import { inject as service } from '@ember/service';
 export default Component.extend({
   tagName: '',
   api: service('game-api'),
+  session: service(),
+
+  didReceiveAttrs() {
+    this._super(...arguments);
+    let viewerId = this.get('session.data.authenticated.id');
+    let participants = this.get('scene.participants') || [];
+    let isParticipant = participants.some((participant) => {
+      return `${participant.id}` === `${viewerId}`;
+    });
+    let custom = this.custom || {};
+    this.set(
+      'canViewSheets',
+      !!custom.soul_can_manage_soul ||
+        (!!custom.soul_can_review_rolls && isParticipant)
+    );
+  },
 
   actions: {
     async lookupBnb() {
