fcbe5f5f461f6a8bfd9cbb8050da2d8f94496b1c
MischiefMaker <59433210+MischiefMaker@users.noreply.github.com>
2026-07-23 17:18:55 -0400

    Complete command parity audit fixes

--- plugin/spec/soul_xp_web_handler_spec.rb
+++ plugin/spec/soul_xp_web_handler_spec.rb
@@ -12,5 +12,21 @@ module AresMUSH
       allow(SoulCharacterApi).to receive(:get_skill_rating).and_return(0)
       expect(subject.handle(request)[:preview]).to be true
     end
+
+    it "surfaces a failed scene award as an error like the MUSH command" do
+      staff = Fabricate(:character)
+      recipient = Fabricate(:character)
+      scene = double(id: 12)
+      request = double(cmd: "soulXpScene", enactor: staff,
+        args: { 'scene_id' => '12', 'amount' => 2, 'reason' => 'scene',
+                'confirmed' => 'true' })
+      allow(Website).to receive(:check_login).and_return(nil)
+      allow(Soul).to receive(:can_manage_soul?).and_return(true)
+      allow(Scene).to receive(:[]).with('12').and_return(scene)
+      allow(SoulXpApi).to receive(:get_scene_participants).and_return([recipient])
+      allow(SoulXpApi).to receive(:award).and_return(error: "award failed")
+
+      expect(subject.handle(request)).to eq(error: "award failed")
+    end
   end
 end
