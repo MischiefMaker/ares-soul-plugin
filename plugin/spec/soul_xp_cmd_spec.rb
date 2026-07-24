80b731795f16895e2488884a33b7bd67193cfbea
MischiefMaker <59433210+MischiefMaker@users.noreply.github.com>
2026-07-24 12:28:06 -0400

    Final audit: close staff parity and documentation gaps

--- plugin/spec/soul_xp_cmd_spec.rb
+++ plugin/spec/soul_xp_cmd_spec.rb
@@ -6,5 +6,13 @@ module AresMUSH
       cmd = double(root: "xp")
       expect(Soul.get_cmd_handler(nil, cmd, nil)).to eq(Soul::SoulXpCmd)
     end
+
+    it "treats +xp/reverse as a staff operation" do
+      command = double(switch: "reverse")
+      allow(subject).to receive(:cmd).and_return(command)
+      allow(subject).to receive(:enactor).and_return(Fabricate(:character))
+      allow(Soul).to receive(:can_manage_soul?).and_return(false)
+      expect(subject.check_permission).to be_present
+    end
   end
 end
